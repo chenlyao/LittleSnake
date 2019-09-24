d1d8c4e5884c0cdfaa1dbcd95face4205dfff1f5
陈陆瑶 <1425798713@qq.com>
2019-09-24 21:44:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wang/game/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.example.wang.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import static javafx.scene.input.KeyCode.R;
+
+
+public class AboutActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutgame);
+    }
+}
